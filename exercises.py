453247c6bcad1a07ca08904d7934c2f111701d2c
CandicePersil <candice.persil@gmail.com>
2020-07-22 00:26:42 +0200

    Update exercices.py
    All three exercices are filled and python ./main command is running successfully. 
    These exercices include two libraries: sys and collections.

--- exercises.py
+++ exercises.py
@@ -1,3 +1,6 @@
+import sys
+import collections
+
 def is_anagram(s1, s2):
     """
     Write an algorithm that returns whether s1 and s2 are anagrams of each other, i.e.
@@ -8,8 +11,28 @@ def is_anagram(s1, s2):
     :return: True or False
     """
 
-    # Write your code here
-    pass
+    try:
+        bool_s1 = isinstance(s1, str)
+        bool_s2 = isinstance(s2, str)
+        # check s1 and s2 type
+        if bool_s1 and bool_s2:
+            # check s1 and s2 length
+            if len(s1) == len(s2):
+                # create a lists for s1 and s2
+                list_s1 = list(s1)
+                list_s2 = list(s2)
+                list_s1.sort()
+                list_s2.sort()
+                # compare list_string_one and list_string_two
+                return True if list_s1 == list_s2 else False
+            else:
+                return False
+        else:
+            # raise exception if var are not str
+            raise Exception(s1, s2)
+    except Exception as e:
+        print("Program stopped. {}: Values aren't both str -> {}.".format(type(e), e))
+        sys.exit(1)
 
 
 def check_parenthesis_consistency(string):
@@ -23,8 +46,30 @@ def check_parenthesis_consistency(string):
     :return: True if the parentheses are balanced, False if not.
     """
 
-    # Write your code here
-    pass
+    try:
+        # check string type
+        if isinstance(string, str):
+            start_char = '('
+            end_char = ')'
+            # compare number of chars '(' and ')' in string
+            if string.count(start_char) == string.count(end_char):
+                counter = 0
+                for char in string:
+                    if char == start_char:
+                        counter += 1
+                    if char == end_char:
+                        counter -= 1
+                    if counter < 0:
+                        return False
+                return True if counter == 0 else False
+            else:
+                return False
+        else:
+            # raise exception if var is not str
+            raise Exception(string)
+    except Exception as e:
+        print("Program stopped. {}: Value isn't str -> {} is {}.".format(type(e), e, type(string)))
+        sys.exit(1)
 
 
 def shortest_path(start, end, maze):
@@ -39,5 +84,33 @@ def shortest_path(start, end, maze):
     :return: list of positions [(x1, y1), (x2, y2), ...] representing the shortest path in the maze
     """
 
-    # Write your code here
+    # define row and column size
+    x_size = len(maze)
+    y_size = len(maze[0])
+    # define a list-like container starting with list of start tuple
+    queue = collections.deque([[start]])
+    # define a set of data starting with start tuple
+    seen = set([start])
+    # create a list including all found path
+    path_list = list()
+    while queue:
+        # for first iteration -> init path with list containing start tuple
+        # then replace it by list of path tuples
+        path = queue.popleft()
+        path_list.append(path)
+        # get the last tuple in path
+        x, y = path[-1]
+        # scan and assign x and y with possible values
+        for x_coord, y_coord in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+            # avoid IndexError
+            if 0 <= x_coord < x_size and 0 <= y_coord < y_size:
+                # avoid walls and already met tuples
+                if maze[x_coord][y_coord] != 0 and (x_coord, y_coord) not in seen:
+                    queue.append(path + [(x_coord, y_coord)])
+                    seen.add((x_coord, y_coord))
+    # in path list we return the first list containing end tuple
+    for shortest_list in path_list:
+        if shortest_list[-1] == end:
+            print(shortest_list)
+            return shortest_list
     pass
